9b88c8a84f5bc9ede23953be32036466ac37ff85
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-04-06 17:03:47 -0500

    Update RC_servo_python.py

--- RC_servo/RC_servo_python.py
+++ RC_servo/RC_servo_python.py
@@ -11,7 +11,6 @@ while True:
     # pulse 0.5 ms to 2.5 ms out of a possible 20 ms (50Hz)
     # for 0 degrees to 180 degrees
     # so duty_cycle can be 65535*0.5/20 to 65535*2.5/20
-    # but check this, some servo brands might only want 1-2 ms
     
     # 0 to 180 degrees
     for i in range(duty_lower, duty_higher, 20):
