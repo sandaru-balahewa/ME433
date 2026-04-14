53413a3bb8d97d7565bc81db732cadb1dd77a682
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-04-14 00:13:59 -0500

    Update HW3.c

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -2,18 +2,32 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
+#define LED_DEBUG 18
+#define CHIP_ADDRESS 0b0100000
+
 // I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 4
 #define I2C_SCL 5
 
+void set_pin(unsigned char address, unsigned char reg, unsigned char val){
+    unsigned char buf[2] = {reg, val};
+    i2c_write_blocking(i2c_default, address, buf, 2, false);
+}
 
+unsigned char read_pin(unsigned char address, unsigned char reg){
+    unsigned char buf;
+    i2c_write_blocking(i2c_default, address, &reg, 1, true);
+    i2c_read_blocking(i2c_default, address, &buf, 1, false);
+}
 
 int main()
 {
     stdio_init_all();
+
+    gpio_init(LED_DEBUG);
+    gpio_set_dir(LED_DEBUG, GPIO_OUT);
+    gpio_put(LED_DEBUG, 0);
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -21,10 +35,12 @@ int main()
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
 
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
-
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // blinking led on pico for debugging purposes
+        gpio_put(LED_DEBUG, 1);
+        sleep_ms(10);
+        gpio_put(LED_DEBUG, 0);
+
+
     }
 }
