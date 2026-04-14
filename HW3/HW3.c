76f1db83ef64d56695b1a3441c1895bcfb8d326b
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-04-14 00:46:10 -0500

    Update HW3.c

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -15,13 +15,13 @@
 
 void set_pin(unsigned char address, unsigned char reg, unsigned char val){
     unsigned char buf[2] = {reg, val};
-    i2c_write_blocking(i2c_default, address, buf, 2, false);
+    i2c_write_blocking(I2C_PORT, address, buf, 2, false);
 }
 
 unsigned char read_pin(unsigned char address, unsigned char reg){
     unsigned char buf;
-    i2c_write_blocking(i2c_default, address, &reg, 1, true);
-    i2c_read_blocking(i2c_default, address, &buf, 1, false);
+    i2c_write_blocking(I2C_PORT, address, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, address, &buf, 1, false);
     return buf;
 }
 
@@ -58,12 +58,12 @@ int main()
         if ((temp_reg & 1) == 0){
             // turn on led
             set_pin(CHIP_ADDRESS, OLAT_REG, 0b10000000);
-            sleep_ms(100);
-            set_pin(CHIP_ADDRESS, OLAT_REG, 0x00000000);
         }
         else{
-            sleep_ms(100);
+            set_pin(CHIP_ADDRESS, OLAT_REG, 0x00000000);
         }
+
         state = !state;
+        sleep_ms(100);
     }
 }
