64806ef6e86501a4693a554d637c9ac20877cffd
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-04-21 00:16:51 -0500

    added draw_message function and fps and adc read functionality

--- HW4_OLED/HW4_OLED.c
+++ HW4_OLED/HW4_OLED.c
@@ -2,6 +2,7 @@
 #include "ssd1306.h"
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 #include "font.h"
 
 #define LED_DEBUG 18
@@ -22,7 +23,24 @@ void draw_letter(unsigned char x, unsigned char y, char letter){
             ssd1306_drawPixel(x + i, y + j, bit);
         }
     }
-    ssd1306_update();
+}
+
+void draw_message(unsigned char x, unsigned char y, char *message){
+    int i = 0;
+    unsigned char new_x = x;
+    unsigned char new_y = y;
+    while (message[i] != 0){
+        draw_letter(new_x, new_y, message[i]);
+        i++;
+        if (new_x + 9 > 127){
+            new_x = 0;
+            new_y = new_y + 8;
+        }
+        else {
+            new_x = new_x + 5;
+        }
+        
+    }
 }
 
 int main()
@@ -46,24 +64,60 @@ int main()
     ssd1306_clear();
     ssd1306_update();
 
+    // initialize adc
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+
     // debug led state
     bool led_state = true;
 
     // test pixel state
     unsigned char pix_state = 1;
 
+    absolute_time_t t_start, t_end;
+    uint64_t t;
+
     while (true) {
+        // get start time
+        t_start = get_absolute_time();
+        ssd1306_clear();
+
         // blinking led on pico for debugging purposes
         gpio_put(LED_DEBUG, led_state);
         led_state = !led_state;
 
-        // // blinking a test pixel
-        // ssd1306_drawPixel(TEST_PIXEL_X, TEST_PIXEL_Y, pix_state);
-        // ssd1306_update();
-        // pix_state = !pix_state;
+        // Read ADC
+        uint16_t pot = adc_read();
+        float voltage = pot * 3.3 / 4096.0;
 
-        // char let = 'A';
-        draw_letter(TEST_PIXEL_X, TEST_PIXEL_Y, 'B');
-        sleep_ms(1000);
+        char line1[30];
+        char line2[30];
+        char line3[30];
+
+        sprintf(line1, "ADC0 voltage is %.2f V", voltage);
+        sprintf(line2, "Go! U Northwestern!");
+        sprintf(line3, "Fight for victory.");
+
+        // write to oled screen
+        draw_message(0, 0, line1);
+        draw_message(0, 8, line2);
+        draw_message(0, 16, line3);
+        ssd1306_update();
+
+        // get end time
+        t_end = get_absolute_time();
+
+        t = to_us_since_boot(t_end) - to_us_since_boot(t_start);
+
+        // calculate fps
+        float fps = 1.0 / (t / 1000000.0);
+        char speed[30];
+        sprintf(speed, "FPS = %6.3f  ", fps);
+
+        // write the fps value to screen
+        draw_message(0, 24, speed);
+        ssd1306_update();
+        sleep_ms(100);
     }
 }
