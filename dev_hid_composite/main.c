eed4962b5c6fbc03821f4b6177dc33cdd93259ea
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-04-23 15:20:50 -0500

    Added the imu data based mouse functionality

--- dev_hid_composite/main.c
+++ dev_hid_composite/main.c
@@ -216,6 +216,12 @@ static void send_hid_report(uint8_t report_id, uint32_t btn)
       }
       else{
         // Use IMU data
+        read_imu_data(imu_raw_data);
+        process_imu_data(imu_proc_data, imu_raw_data);
+        
+        printf("X_Accel: %i, Y_Accel: %i, Z_Accel: %i\n", imu_proc_data[0], imu_proc_data[1], imu_proc_data[2]);
+        deltax = (int8_t) (imu_proc_data[0]/32768.0*20.0) * (-1);
+        deltay = (int8_t) (imu_proc_data[1]/32768.0*20.0);
       }
 
       // no button, right + down, no scroll, no pan
