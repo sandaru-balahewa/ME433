5f44434c69c1b9fdb78ec49cd5a0bbb7e3099512
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-05-07 22:54:45 -0500

    Update HW9.py

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -5,10 +5,12 @@ import numpy as np
 # List to store the paths of 4 files
 file_list = ["sigA.csv", "sigB.csv", "sigC.csv", "sigD.csv"]
 
-t = []
-sig_data = []
-
 for file in file_list:
+
+    # Lists to store time and signal data
+    t = []
+    sig_data = []
+
     with open(file) as f:
         # open the csv file
         reader = csv.reader(f)
@@ -17,22 +19,21 @@ for file in file_list:
             t.append(float(row[0])) # leftmost column
             sig_data.append(float(row[1])) # second column
 
-    sample_rate = (len(t) - 1) / t[-1]
+    # # convert to numpy arrays
+    # t = np.array(t)
+    # sig_data = np.array(sig_data)
+
+    # Calculate sample rate
+    sample_rate = (len(t) - 1) / (t[-1] - t[0])
+
     print(sample_rate)
-    
+
     # FFT
 
-
-
-    dt = 1.0/10000.0 # 10kHz
-    t = np.arange(0.0, 1.0, dt) # 10s
-    # a constant plus 100Hz and 1000Hz
-    s = 4.0 * np.sin(2 * np.pi * 100 * t) + 0.25 * np.sin(2 * np.pi * 1000 * t) + 25
-
-    Fs = 10000 # sample rate
-    Ts = 1.0/Fs; # sampling interval
-    ts = np.arange(0,t[-1],Ts) # time vector
-    y = s # the data to make the fft from
+    Fs = sample_rate # sample rate
+    # Ts = 1.0/Fs; # sampling interval
+    # ts = np.arange(0,t[-1],Ts) # time vector
+    y = sig_data # the data to make the fft from
     n = len(y) # length of the signal
     k = np.arange(n)
     T = n/Fs
@@ -48,6 +49,7 @@ for file in file_list:
     ax2.loglog(frq,abs(Y),'b') # plotting the fft
     ax2.set_xlabel('Freq (Hz)')
     ax2.set_ylabel('|Y(freq)|')
+    plt.tight_layout()
     plt.show()
 
 
