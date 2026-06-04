d47e68d861a5924fbb087ff15576ce8ab072c918
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-06-03 19:20:52 -0500

    Update library.h

--- HW16/FDCAN_Com_Polling/Inc/library.h
+++ HW16/FDCAN_Com_Polling/Inc/library.h
@@ -12,5 +12,12 @@
 #include "stm32c0xx_hal.h"
 
 uint32_t read_adc(ADC_HandleTypeDef *hadc);
+void init_ina219();
+float read_ina219();
+void writeINA219(int reg, int value);
+signed short readINA219(unsigned char reg);
+
+
+
 
 #endif /* APPLICATION_USER_LIBRARY_H_ */
