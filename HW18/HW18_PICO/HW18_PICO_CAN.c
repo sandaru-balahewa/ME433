2f2fb49d0e656365d1dbd239f60bfb56d961b998
sandaru-balahewa <118431796+sandaru-balahewa@users.noreply.github.com>
2026-06-10 15:37:32 -0500

    Update HW18_PICO_CAN.c

--- HW18/HW18_PICO/HW18_PICO_CAN.c
+++ HW18/HW18_PICO/HW18_PICO_CAN.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "hardware/spi.h"
 
+
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
